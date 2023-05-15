bc9e1de963ae9a0ef151d2bc2f4d705ca0b0bed9
Pr1ntel <vandushev.sergey@yandex.ru>
2023-05-15 19:20:55 +0300

    create project. configuration Pom

---
+++ src/main/java/com/example/library_films_backend/LibraryFilmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_films_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryFilmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryFilmsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/library_films_backend/LibraryFilmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_films_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryFilmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
