97ea434cf487beb4b5032d5edbd98e916e546a86
Pr1ntel <vandushev.sergey@yandex.ru>
2023-05-22 23:02:07 +0300

    auth and register 30%

--- src/main/java/com/example/library_films_backend/config/SecurityConfiguration.java
+++ src/main/java/com/example/library_films_backend/config/SecurityConfiguration.java
@@ -21,6 +21,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(corsFilter, SessionManagementFilter.class)
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
--- src/main/java/com/example/library_films_backend/jwt_controllers/SecureController.java
+++ src/main/java/com/example/library_films_backend/jwt_controllers/SecureController.java
@@ -1,21 +1,17 @@
 package com.example.library_films_backend.jwt_controllers;
 
-import com.example.library_films_backend.model.FilmsItem;
-import com.example.library_films_backend.service.FilmsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/secure")
 public class SecureController {
-    private final FilmsService filmsService;
-    @GetMapping("/all-films")
-        public List<FilmsItem> getAll() {
-            return filmsService.getAll();
+
+    @GetMapping("/home")
+    public String getHello() {
+        return "hello secure";
     }
 }
